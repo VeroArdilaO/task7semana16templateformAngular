de768f0a3cf2341ec860a0515efb1e2870a6391b
Veronica <veronica.ardilao@outlook.es>
2021-06-27 18:42:10 -0500

    se creo un nuevo template-driven-form para la task 7 semana 16 Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CountryComponent } from './country/country.component';
 import { HomeComponent } from './home/home.component';
 import { BlogComponent } from './blog/blog.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CountryComponent,
     HomeComponent,
-    BlogComponent
+    BlogComponent,
+    
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { IPost } from '../interafaces';
+import { IForm } from '../interafaces/blog.interace';
 import { PostService } from '../services';
+
+
 
 @Component({
   selector: 'app-blog',
@@ -8,13 +11,32 @@ import { PostService } from '../services';
   styleUrls: ['./blog.component.css'],
 })
 export class BlogComponent implements OnInit {
+  
   public posts: IPost[] = [];
+
+  form: IForm = {
+    id: 0,
+    title:"Add text",
+    body:"Add text"
+  }
 
   constructor(private _postService: PostService) {}
 
   ngOnInit(): void {
     this._postService
       .getPosts()
-      .subscribe((posts) => (this.posts = posts));
+      .subscribe((posts) => (this.posts = posts.slice(0,3)));
+  }
+
+  save():void{
+
+    console.log(JSON.stringify(this.form))
+    const newId = this.form.id
+    const newTitle = this.form.title
+    const newBody = this.form.body
+    this.posts.push({id: newId, title: newTitle, body:newBody})
+    this.form.id = 0,
+    this.form.title = "add text",
+    this.form.body = "add text"
   }
 }
